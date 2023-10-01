75e0eae9a6098626311a642f42da5317e26b6e28
Muhammad Eisa <118332655+eisakhan04@users.noreply.github.com>
2023-10-02 01:49:50 +0500

    Add files via upload

---
+++ Simple calculator.php
@@ -0,0 +1,28 @@
+<?php
+ 
+ $x = 10 ;
+
+ $y = 30 ;
+
+ $z = "/";
+
+ if($z == "+")
+   echo $x +  $y;
+
+ elseif($z == "-")  
+   echo $x -  $y;
+
+ elseif($z == "*")  
+   echo $x *  $y;
+  
+ elseif($z == "/")  
+   echo $x /  $y;
+
+ elseif($z == "%")  
+   echo $x %  $y;   
+else
+   echo "invalid";
+
+
+
+?>
