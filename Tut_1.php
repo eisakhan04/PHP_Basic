b12e4c84deb8fba0c33b1ee7258073575a7c3565
Muhammad Eisa <118332655+eisakhan04@users.noreply.github.com>
2023-10-02 02:28:53 +0500

    Add files via upload

---
+++ Tut_1.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP coding style</title>
+</head>
+<body>
+    <?php 
+    $color = "black";
+    echo "My car is ".$color."</br>";
+    echo "My dog is ".$color."</br>";
+    echo "My Phone  is ".$color."</br>";
+   
+    ?>
+   
+</body>
+</html>
